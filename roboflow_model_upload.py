f1c7aea96ff9e97c42813df3c354ab58f02a27a8
AldeR <koalder@yandex.ru>
2025-03-26 01:34:57 +0300

    Теперь код рабочий

--- roboflow_model_upload.py
+++ roboflow_model_upload.py
@@ -4,13 +4,12 @@ from roboflow import Roboflow
 rf = Roboflow(api_key="v3cFAranxYzTNutcgON4")
 
 # Получаем проект
-project = rf.workspace("rye-jo3uz").project("my-first-project-7i8pk")
-
-# Указываем версию проекта (замените VERSION_NUMBER на номер версии, например, 1)
-version = project.version(1)  # Убедитесь, что версия существует в вашем проекте
+workspace = rf.workspace("rye-jo3uz")
 
 # Загружаем веса модели
-version.deploy(
+workspace.deploy_model(
     model_type="yolov8-obb",  # Тип модели (например, yolov8-obb)
-    model_path="last.pt"      # Путь к файлу с весами модели
+    model_path="best.pt",      # Путь к файлу с весами модели
+    project_ids=['my-first-project-7i8pk'],
+    model_name="last"
 )
