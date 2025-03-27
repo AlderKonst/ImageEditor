c6675f7fd2f0149a4cc7c9d33698379e15c71f20
Я <koalder@yandex.ru>
2025-03-27 09:37:21 +0300

    Добавил ссылку

--- roboflow_model_upload.py
+++ roboflow_model_upload.py
@@ -8,8 +8,9 @@ workspace = rf.workspace("rye-jo3uz")
 
 # Загружаем веса модели
 workspace.deploy_model(
-    model_type="yolov8-obb",  # Тип модели (например, yolov8-obb)
+    model_type="yolov11-obb",  # Тип модели (например, yolov8-obb)
     model_path="best.pt",      # Путь к файлу с весами модели
     project_ids=['my-first-project-7i8pk'],
     model_name="last"
 )
+'https://detect.roboflow.com/my-first-project-7i8pk/last?api_key=v3cFAranxYzTNutcgON4'
